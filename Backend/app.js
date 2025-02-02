b164a546a6fd668649ff355c081b50cc527f8a9f
migueldev2006 <gironorozcoluismiguel3@gmail.com>
2025-02-01 20:11:01 -0500

    Creacion de la estructura basica de una API para el Proyecto Formativo

---
+++ Backend/app.js
@@ -0,0 +1,12 @@
+import express from "express"
+import bodyParser from "body-parser";
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.listen(3000, () => {
+    console.log("API activa en el servidor 3000")
+});
